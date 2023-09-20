9b6ca48df5721d04ec09afcb52685d55f66a1dfb
Qiusheng Wu <giswqs@gmail.com>
2023-09-20 08:43:16 -0400

    Add additional parameters for folium layer

--- folium_pmtiles/vector.py
+++ folium_pmtiles/vector.py
@@ -31,10 +31,12 @@ class PMTilesVectorBaseMap(JSCSSMixin, Layer):
         )
     ]
 
-    def __init__(self, url, layer_name=None, options=None):
+    def __init__(self, url, layer_name=None, options=None, **kwargs):
+
+        
         self.layer_name = layer_name if layer_name else "PMTilesVector"
 
-        super().__init__(name=self.layer_name)
+        super().__init__(name=self.layer_name, **kwargs)
 
         self.url = url
         self._name = "PMTilesVector"
