cef6efda926668c0caf99f0ae786a163a7cf754e
Jt Miclat <jtmiclat@gmail.com>
2023-11-18 10:48:04 +0800

    Add initial implementation of tooltip

--- folium_pmtiles/vector.py
+++ folium_pmtiles/vector.py
@@ -67,7 +67,8 @@ class PMTilesMapLibreLayer(JSCSSMixin, Layer):
 
             var {{ this.get_name() }} = L.maplibreGL({
             pane: 'overlay',
-            style: {{ this.style|tojson}}
+            style: {{ this.style|tojson}},
+            interactive: true,
             }).addTo({{ this._parent.get_name() }});
 
             {%- endmacro %}
@@ -86,15 +87,55 @@ class PMTilesMapLibreLayer(JSCSSMixin, Layer):
         ),
     ]
 
-    def __init__(self, url, layer_name=None, style=None, **kwargs):
+    def __init__(self, url, layer_name=None, style=None, tooltip=None, **kwargs):
         self.layer_name = layer_name if layer_name else "PMTilesVector"
 
         super().__init__(name=self.layer_name, **kwargs)
 
         self.url = url
         self._name = "PMTilesVector"
-
+        if tooltip is not None:
+            self.add_child(tooltip)
         if style is not None:
             self.style = style
         else:
             self.style = {}
+
+
+class PMTilesMapLibreTooltip(JSCSSMixin, Layer):
+    _template = Template(
+        """
+            {% macro header(this, kwargs) %}
+            <style>
+            .maplibregl-popup {
+                max-width: 400px;
+                font: 12px/20px 'Helvetica Neue', Arial, Helvetica, sans-serif;
+                z-index: 651;
+            }
+            </style>
+            {% endmacro %}
+
+            {% macro script(this, kwargs) -%}
+                var {{ this.get_name() }} = {{ this._parent.get_name() }}.getMaplibreMap();
+                const popup = new maplibregl.Popup({
+                    closeButton: false,
+                    closeOnClick: false
+                });
+                {{ this.get_name() }}.on('load', () => {
+                    {{ this.get_name() }}.on('mousemove', {{ this.layer | tojson }}, (e) => { 
+                        {{ this.get_name() }}.getCanvas().style.cursor = 'pointer';
+                        const {lng, lat}  = e.lngLat;
+                        const coordinates = [lng, lat]
+                        const description = JSON.stringify( e.features[0].properties);
+                        popup.setLngLat(coordinates).setHTML(description).addTo({{ this.get_name() }});
+                    });
+                });
+            {%- endmacro %}
+            """
+    )
+
+    def __init__(self, name=None, layer=None):
+        super().__init__(
+            name=name if name else "PMTilesTooltip",
+        )
+        self.layer = layer
